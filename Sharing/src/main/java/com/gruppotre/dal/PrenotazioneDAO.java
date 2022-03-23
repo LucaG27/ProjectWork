d495f8110b9f274fadd4bc34d14cf061cbd50622
LucaG27 <96395783+LucaG27@users.noreply.github.com>
2022-03-23 16:39:25 +0100

    noData

--- Sharing/src/main/java/com/gruppotre/dal/PrenotazioneDAO.java
+++ Sharing/src/main/java/com/gruppotre/dal/PrenotazioneDAO.java
@@ -10,7 +10,5 @@ public interface PrenotazioneDAO extends JpaRepository<Prenotazione, Integer>{
 
 	List<Prenotazione> findAllByStato(String stato);
 	
-	List<Prenotazione> findAllByData(String data);
-
 	Prenotazione findById(int id);
 }
