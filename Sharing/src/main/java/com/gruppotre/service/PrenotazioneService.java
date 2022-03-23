64f49bc1c23012a3fb5b50f10ccbaaed7b84ef09
LucaG27 <96395783+LucaG27@users.noreply.github.com>
2022-03-23 16:08:29 +0100

    Fix

--- Sharing/src/main/java/com/gruppotre/service/PrenotazioneService.java
+++ Sharing/src/main/java/com/gruppotre/service/PrenotazioneService.java
@@ -3,7 +3,6 @@ package com.gruppotre.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import com.gruppotre.dal.PrenotazioneDAO;
@@ -14,12 +13,6 @@ public class PrenotazioneService {
 
 	@Autowired
 	PrenotazioneDAO pdb;
-
-	@Query(value="select ordine.id, cliente_id, impiegato_id, data, consegna, sum(quantita*prezzo) somma "
-            + "from ordine, ordine_dettaglio, articolo "
-            + "where ordine.id=ordine_dettaglio.ordine_id AND articolo.id=ordine_dettaglio.articolo_id "
-            + "group by ordine.id; ", nativeQuery=true)
-    List<Prenotazione> getPrenotazioni(int id);
 	
 	public List<Prenotazione> findByStato(String stato) {
 		return pdb.findByStato(stato);
