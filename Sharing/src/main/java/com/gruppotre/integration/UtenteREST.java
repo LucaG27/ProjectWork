33c406c851126e9045fa4a9f4180d7a825e01a85
Raffaele Del Prete <raffaele93delprete@gmail.com>
2022-03-24 15:37:19 +0100

    update panello di controllo

--- Sharing/src/main/java/com/gruppotre/integration/UtenteREST.java
+++ Sharing/src/main/java/com/gruppotre/integration/UtenteREST.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +38,11 @@ public class UtenteREST {
 	@GetMapping("/control")
 	public ResponseEntity<Utente> control(HttpSession session){
 		return new ResponseEntity<Utente>((Utente) session.getAttribute("utenteSession"), HttpStatus.OK);
+	}
+
+	@GetMapping("/utente/{email}")
+	public Utente getUtente(@PathVariable("email") String email){
+		return srv.getUtente(email);
 	}
 
 }
