389cd557512f4c255e61a195afdee1d3866787b1
Aleinder <aleinder@libero.it>
2022-03-25 10:17:27 +0100

    First Upd utente.html

--- Sharing/src/main/resources/static/js/utente.js
+++ Sharing/src/main/resources/static/js/utente.js
@@ -5,6 +5,10 @@ const user = localStorage.getItem('user');
 let render_prenotazioni = null;
 let render_prenotazioni2 = null;
 
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
 function listaPrenotazioni(event){
 
     fetch(URL+"prenotazioni/"+ "1")
@@ -19,13 +23,12 @@ function listaPrenotazioni(event){
             let rows2 = "";       
             rows = render_prenotazioni(json);
             rows2 = render_prenotazioni2(json);
-            for(let li=0; li<json.length; li++){
-            if(json[li].stato == "in corso"){
-                document.getElementById("bodyTabellaClienti").innerHTML = rows;
-            }else{
-                document.getElementById("bodyTabellaClienti2").innerHTML = rows2;
-            }
-        }
+           
+            document.getElementById("bodyTabellaClienti").innerHTML = rows;
+            document.getElementById("bodyTabellaClienti2").innerHTML = rows2;
+       
+    
+        
         })
         .catch(function(err) { 
                 alert(err);
