4037e54f5cd6b9c726f85c4eb5700e8c63b09428
Raffaele Del Prete <raffaele93delprete@gmail.com>
2022-03-25 12:00:53 +0100

    mod veicolo js e auto html

--- Sharing/src/main/resources/static/js/veicolo.js
+++ Sharing/src/main/resources/static/js/veicolo.js
@@ -1,5 +1,7 @@
+let render_specifiche = null;
 let render_veicoli = null;
 const URL = "http://localhost:8080/api/immagine/allImmagini"
+const URL2 = "http://localhost:8080/api/specifiche/"
 const user = localStorage.getItem('user');
   "use strict";
 
@@ -25,14 +27,62 @@ const user = localStorage.getItem('user');
           });	
   
   }
+
+    
+  function listaSpecifiche(event){
+  
+    fetch(URL2+"id/"+5)
+        .then(function(response) {
+        return response.json();
+      })
+        .then(function(json) {
+      
+            console.log(json);
+    
+            let rows = "";      
+            rows = render_specifiche(json);
+                document.getElementById("body_specifiche").innerHTML = rows;
+        })
+        .catch(function(err) { 
+                alert(err);
+                console.log('Failed to fetch page: ', err);
+        });	
+
+}
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
   
   window.addEventListener(
     'DOMContentLoaded', 
     function(event){
   
       render_veicoli = Handlebars.compile( document.getElementById("template-veicoli").innerHTML );
+      render_specifiche = Handlebars.compile( document.getElementById("template-specifiche").innerHTML );
   
           listaVeicoli();
+          listaSpecifiche();
       
   });
 
