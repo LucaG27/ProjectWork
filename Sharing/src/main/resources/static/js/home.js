814b11a647f8138e88ba501baaf32a4057005cc0
LeoGiardino <panto.giardino@gmail.com>
2022-03-25 10:52:51 +0100

    update

--- Sharing/src/main/resources/static/js/home.js
+++ Sharing/src/main/resources/static/js/home.js
@@ -57,3 +57,15 @@ const swiper = new Swiper('.swiper', {
   myModal.addEventListener('shown.bs.modal', function () {
     myInput.focus()
   })
+
+
+  const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#password');
+ 
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
