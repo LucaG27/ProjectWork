ad5ce108c0091d34d8e9c614d7b0e706e936da4c
Raffaele Del Prete <raffaele93delprete@gmail.com>
2022-03-25 08:52:32 +0100

    upd utente

--- Sharing/src/main/resources/static/js/utente.js
+++ Sharing/src/main/resources/static/js/utente.js
@@ -4,17 +4,16 @@ const URL = "http://localhost:8080/api/veicolo/";
 const user = localStorage.getItem('user');
 let render_prenotazioni = null;
 let render_prenotazioni2 = null;
+let session = JSON.parse(localStorage.getItem('user'))
 
 function listaPrenotazioni(event){
 
-    fetch(URL+"prenotazioni/"+ "1")
+    fetch(URL+"prenotazioni/"+ session.id)
         .then(function(response) {
 		    return response.json();
 	    })
         .then(function(json) {
-			
             console.log(json);
-    
             let rows = "";   
             let rows2 = "";       
             rows = render_prenotazioni(json);
