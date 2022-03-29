5c722bc9653f13f3bf47b09ac1bca830ade344b6
Raffaele Del Prete <raffaele93delprete@gmail.com>
2022-03-29 17:41:56 +0200

    upd

--- Sharing/src/main/java/com/gruppotre/integration/VeicoloREST.java
+++ Sharing/src/main/java/com/gruppotre/integration/VeicoloREST.java
@@ -3,13 +3,16 @@ package com.gruppotre.integration;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gruppotre.entity.Veicolo;
 import com.gruppotre.entity.VeicoloPrenotazioniDTO;
@@ -55,5 +58,12 @@ public class VeicoloREST {
 	@GetMapping("/prenotazioni/{id}")
 	public List<VeicoloPrenotazioniDTO> getAllPrenotazioni(@PathVariable("id") int id){
 		return srv.getAllPrenotazioni(id);
+	}
+	
+	@PostMapping("/import")
+	public ResponseEntity insertCSV(@RequestPart("fileCSV") MultipartFile file) {
+		
+		srv.importCsv(file);
+		return ResponseEntity.ok().build();	
 	}
 }
