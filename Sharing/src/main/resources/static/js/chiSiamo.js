ec5087109295e14bebce2cbc341758ba19760dcf
LucaG27 <96395783+LucaG27@users.noreply.github.com>
2022-03-30 10:47:58 +0200

    ChiSiamo

---
+++ Sharing/src/main/resources/static/js/chiSiamo.js
@@ -0,0 +1,43 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1, //valore di default che funziona fuori dai breakpoint (da 0px)
+  spaceBetween: 10,
+  breakpoints: {
+    // when window width is >= 576
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  },
+  autoplay: {
+    delay: 2300,
+    disableOnInteraction: false,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  });
