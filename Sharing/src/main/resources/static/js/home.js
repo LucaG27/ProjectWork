e61b1301731819e7486cda664d367962b7f7089b
LeoGiardino <panto.giardino@gmail.com>
2022-03-31 09:20:16 +0200

    agg

--- Sharing/src/main/resources/static/js/home.js
+++ Sharing/src/main/resources/static/js/home.js
@@ -17,6 +17,43 @@ let info= null;
 const URL3 = "http://localhost:8080/api/veicolo/id/"
 const URL4 = "http://localhost:8080/api/prenotazioni/"
 
+var util = {
+  mobileMenu() {
+    $("#nav").toggleClass("nav-visible");
+  },
+  windowResize() {
+    if ($(window).width() > 800) {
+      $("#nav").removeClass("nav-visible");
+    }
+  },
+  scrollEvent() {
+    var scrollPosition = $(document).scrollTop();
+
+    $.each(util.scrollMenuIds, function (i) {
+      var link = util.scrollMenuIds[i],
+        container = $(link).attr("href"),
+        containerOffset = $(container).offset().top,
+        containerHeight = $(container).outerHeight(),
+        containerBottom = containerOffset + containerHeight;
+
+      if (
+        scrollPosition < containerBottom - 20 &&
+        scrollPosition >= containerOffset - 20
+      ) {
+        $(link).addClass("active");
+      } else {
+        $(link).removeClass("active");
+      }
+    });
+  }
+};
+
+$(document).ready(function () {
+  util.scrollMenuIds = $("a.nav-link[href]");
+  $("#menu").click(util.mobileMenu);
+  $(window).resize(util.windowResize);
+  $(document).scroll(util.scrollEvent);
+});
 
 // handlebars Helpers
 /*
