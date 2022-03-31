defbb69d0c3166d751e727c5a6fe9c92555652b2
LucaG27 <96395783+LucaG27@users.noreply.github.com>
2022-03-31 22:49:54 +0200

    chartJs

--- Sharing/src/main/resources/static/js/chart.js
+++ Sharing/src/main/resources/static/js/chart.js
@@ -1,31 +1,43 @@
+var ctx = document.getElementById('myChart');
 
-  const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ];
+var config = {
+    type: 'bar',
+    data: {
+        labels: ['1900', '1950', '2000', '2018', '2020'],
+        datasets: [{
+            label: "Emissioni CO2 Italia",
+            type: "bar",
+            backgroundColor: "#A7CA9F",
+            borderColor: "#3EA824",
+            borderWidth: 1,
+            fill: false,
+            xAxisID: "axis-bar",
+            data: [130.91, 410.43, 980.49, 1209.02, 1800.82]
+        },
+        {
+            label: "Emissioni CO2 in Europa",
+            type: "bar",
+            backgroundColor: "#E9BCBC",
+            borderColor: "#E9BCBC",
+            borderWidth: 1,
+            fill: true,
+            xAxisID: "axis-bar",
+            data: [1950.0, 2380.0, 5550.0, 6190.0, 4950.0]
+        },
+       /* {
+            label: "Emissioni CO2 nel Mondo",
+            type: "bar",
+            backgroundColor: "#E9E2BC",
+            borderColor: "#E9E2BC",
+            borderWidth: 1,
+            fill: true,
+            xAxisID: "axis-bar",
+            data: [1210.0, 6000.0, 25230.0, 36650.0, 34810.0]
+        }*/]
+    },
 
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-  };
+};
 
-  const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
-
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
- 
+var myChart = new Chart(ctx, config);
+//var legend = myChart.generateLegend();
+//document.getElementById("legend").innerHTML = legend;
