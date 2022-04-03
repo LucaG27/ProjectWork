ae3ebcd4badb3b22f0270c95da7975e1d58a6336
Raffaele Del Prete <raffaele93delprete@gmail.com>
2022-04-03 16:03:30 +0200

    upd veicolo

--- Sharing/src/main/resources/static/js/veicolo.js
+++ Sharing/src/main/resources/static/js/veicolo.js
@@ -213,6 +213,19 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+//OCCHIO
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  // toggle the eye slash icon
+  this.classList.toggle('fa-eye-slash');
+});
+
   
   window.addEventListener(
     'DOMContentLoaded', 
